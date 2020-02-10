3b12e902e2a6bbcd9ec86ed463c5b14f234f6012
Raphael Lullis <raphael@lullis.net>
2020-02-10 12:33:17 +0100

    Add extra check to see if there is another checkout object already in the DOM, and removes it

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import Vue from 'vue'
 import Checkout from './components/Checkout.vue'
 import store from './store'
 
+var CURRENT_CHECKOUT = null
 
 function checkout(selector, settings) {
     let container = document.querySelector(selector)
     if (!container) {
         throw(`${selector} is not a valid document selector`)
+    }
+
+    if (CURRENT_CHECKOUT) {
+        CURRENT_CHECKOUT.$el.remove()
     }
 
     let VueCheckout = Vue.extend(Checkout)
@@ -16,6 +21,8 @@ function checkout(selector, settings) {
 
     this._checkout.$mount()
     container.appendChild(this._checkout.$el)
+
+    CURRENT_CHECKOUT = this._checkout
 }
 
 
